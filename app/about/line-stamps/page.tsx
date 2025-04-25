04fb807736249314543dbea283280ceddbd30bf1
a10adotapp <mail@a10a.app>
2025-04-25 21:31:00 +0900

    edit

--- app/about/line-stamps/page.tsx
+++ app/about/line-stamps/page.tsx
@@ -1,3 +1,5 @@
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@/components/ui/table";
+
 export default async function Page() {
   return (
     <div className="max-w-3xl mx-auto p-4">
@@ -6,9 +8,9 @@ export default async function Page() {
           <div className="flex flex-col gap-4">
             <div className="font-bold">マイスタンプGPT</div>
 
-            <div className="text-gray-400">マイスタンプGPTはAIを使用して人物や動物のスタンプ画像を作成するサービスです。</div>
-
-            <div className="text-gray-400">
+            <div className="text-gray-600">
+              マイスタンプGPTはAIを使用して人物や動物のスタンプ画像を作成するサービスです。
+              <br />
               ご利用は以下のURL、またはQRコードからLINE公式アカウントを友だち追加してください
             </div>
 
@@ -24,6 +26,55 @@ export default async function Page() {
                 https://lin.ee/a5Ozffh
               </a>
             </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <div className="font-bold">特定商取引法に基づく表記</div>
+
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">販売業社の名称</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">マイスタンプGPT</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">所在地</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">請求があったら遅滞なく開示します</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">電話番号</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">請求があったら遅滞なく開示します</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">メールアドレス</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">mail@a10a.app</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">運営統括責任者</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">新垣 克真</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">交換および返品<br />（返金ポリシー）</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">システム不具合によりマイスタンプが提供されない場合、返金いたします。</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">引渡時期</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">注文後、順番にマイスタンプが生成され、提供されます。</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">受け付け可能な<br />決済手段</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">クレジットカード</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">決済期間</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">ただちに処理されます。</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead className="p-2 text-xs text-gray-400 font-bold">販売価格</TableHead>
+                  <TableCell className="p-2 text-xs text-gray-400">¥100</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
           </div>
 
           <div className="flex flex-col gap-4">
